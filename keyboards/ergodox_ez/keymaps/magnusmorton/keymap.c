8a317a9a5ea54d3f6523b3aa9a6963a39cdeeb0f
Magnus Morton <magnus.morton@ed.ac.uk>
2020-04-19 18:15:59 +0100

    colemak full

--- keyboards/ergodox_ez/keymaps/magnusmorton/keymap.c
+++ keyboards/ergodox_ez/keymaps/magnusmorton/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | BS     |   Q  |   W  |   F  |   J  |   G  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |  P   |   \    |
+ * | BS     |   Q  |   W  |   F  |   P  |   G  |  L1  |           |  L1  |   J  |   L  |   U  |   Y  |  ;   |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   R  |   S  |   T  |   D  |------|           |------|   H  |   N  |   E  |   L  |  ;   |'       |
+ * | Ctrl   |   A  |   R  |   S  |   T  |   D  |------|           |------|   H  |   N  |   E  |   I  |  O   |'       |
  * |--------+------+------+------+------+------|  ->  |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   K  |   ,  |   .  |/     | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
   // left hand
   KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,
-  KC_BSPC,          KC_Q,        KC_W,          KC_F,    KC_J,    KC_G,    TG(SYMB),
+  KC_BSPC,          KC_Q,        KC_W,          KC_F,    KC_P,    KC_G,    TG(SYMB),
   KC_LCTL,         KC_A,        KC_R,          KC_S,    KC_T,    KC_D,
   KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    RCKT,
   KC_GRV, KC_COLON,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                          KC_SPC, KC_BSPC, KC_DEL,
   // right hand
   KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_H,         KC_N,    KC_E,    KC_L,    KC_SCLN,           KC_QUOT,
+  TG(SYMB),     KC_J,    KC_L,    KC_U,    KC_Y,              KC_SCLN,           KC_BSLS,
+  KC_H,         KC_N,    KC_E,    KC_I,    KC_O,           KC_QUOT,
   KC_RBRC,      KC_K,    KC_M,    KC_COMM, KC_DOT,            LT(SYMB,KC_SLSH),        KC_RSFT,
   KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
   KC_LALT, CTL_T(KC_ESC),
