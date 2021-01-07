8a7146ac6118a9ced8ba58d72b856a9297dd28f5
Magnus Morton <m@magnusmorton.com>
2021-01-07 23:06:05 +0000

    vim again

--- keyboards/ergodox_ez/keymaps/magnusmorton/keymap.c
+++ keyboards/ergodox_ez/keymaps/magnusmorton/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "keymap_uk.h"
+#include "quantum.h"
 
 #define QWR 0 // default layer
 #define SYMB 1 // symbols
@@ -31,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Win  |  `   |AltShf| Left | Right|                                       |  Up  | Down  | Left | right| ~L1  |
+ *   | Win  |  `   |Alt   | Left | Right|                                       |  Up  | Down  | Left | right| ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Home | End  |       | PrtScr|RAlt  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Del  |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  LALT  |Enter |
- *                                 |      |ace   | Ins  |       | PgDn |        |      |
+ *                                 | Space| BSpc |------|       |------|  Esc   |Enter |
+ *                                 |      |      | lalt |       | lalt |        |      |
  *                                 `--------------------'       `----------------------'
  */
 [QWR] = LAYOUT_ergodox(
@@ -47,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
   KC_LCTL,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_LBRC,
-  KC_LGUI,         UK_GRV,      LALT(KC_LSFT), KC_LEFT, KC_RGHT,
+  KC_LGUI,         UK_GRV,      KC_LALT, KC_LEFT, KC_RGHT,
                                                            KC_HOME, KC_END,
                                                                           KC_DEL,
-                                                         KC_SPC, KC_BSPC, KC_INS,
+                                                         KC_SPC, KC_BSPC, KC_LALT,
   // right hand
   KC_MINS,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           UK_HASH,
   TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           UK_BSLS,
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_UP,        KC_DOWN, KC_LEFT, KC_RIGHT, TT(SYMB),
   KC_PSCREEN,  KC_RALT,
   KC_PGUP,
-  KC_PGDN, KC_LALT, KC_ENT
+  KC_LALT, KC_ESC, KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
